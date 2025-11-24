c45c003aa1a92cdf79ed075bd035835cb57fea4b
marianaakuma <153663415+marianaakuma@users.noreply.github.com>
2025-11-24 20:06:06 +0000

    24/11/25

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+# Inicializando o Flask
+app = Flask(__name__)
+
+# Rotas principais
+@app.route('/')
+def capa():
+    return render_template('capa.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
