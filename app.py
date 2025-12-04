30b368a3535d56ce61192e275aa0f8f92baad366
marianaakuma <153663415+marianaakuma@users.noreply.github.com>
2025-12-04 15:02:13 +0000

    12:02

--- app.py
+++ app.py
@@ -8,6 +8,14 @@ app = Flask(__name__)
 def capa():
     return render_template('capa.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
