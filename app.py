2b8af5be1c1ed66f0d9cb92758235253b42f8310
marianaakuma <153663415+marianaakuma@users.noreply.github.com>
2025-12-05 19:03:34 +0000

    05/12/25

--- app.py
+++ app.py
@@ -8,9 +8,14 @@ app = Flask(__name__)
 def capa():
     return render_template('capa.html')
 
-@app.route('/usuario_create')
-def usuario_create():
-    return render_template('usuario_create.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
